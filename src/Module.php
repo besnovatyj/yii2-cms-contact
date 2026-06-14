8cd5209146f650f5d8aa56a38f743aa6a9eef9d1
besnovatyj <metr31@yandex.ru>
2026-06-15 02:49:46 +0500

    to new modman

--- src/Module.php
+++ src/Module.php
@@ -1,6 +1,5 @@
 <?php
 
-
 /*
  * Copyright (c) 2026 Besnovatyj. Licensed under the MIT License.
  */
@@ -9,16 +8,21 @@ declare(strict_types=1);
 
 namespace Besnovatyj\Contact;
 
-use common\components\module\BaseModule;
+use common\components\module\CmsModule;
+use modules\modmanNew\contract\DeclaresModule;
+use modules\modmanNew\contract\ProvidesAdminMenu;
+use modules\modmanNew\contract\ProvidesMigrations;
+use modules\modmanNew\contract\ProvidesOptions;
 
 /**
  * Модуль контактных форм и адресной книги.
  */
-class Module extends BaseModule
+class Module extends CmsModule implements
+    DeclaresModule, ProvidesAdminMenu,
+    ProvidesMigrations, ProvidesOptions
 {
     public const bool EDITABLE = true;
-
-    /** Идентификатор модуля в приложении */
+    public const string VERSION = '1.0.0';
     public const string MODULE_ID = 'Contact';
 
     /** Методы отправки уведомлений о новых сообщениях */
@@ -28,23 +32,18 @@ class Module extends BaseModule
     public const string SEND_METHOD_TELEGRAM = 'telegram';  // Telegram
     public const string SEND_METHOD_ALL      = 'all';       // Все каналы
 
-    public static function getAdminMenu(): array
-    {
-        return require __DIR__ . '/config/adminMenu.php';
-    }
-
-    public static function getConfig(): array
-    {
-        return require __DIR__ . '/config/config.php';
-    }
-
-    public static function getOptions(): array
-    {
-        return require __DIR__ . '/config/options.php';
-    }
-
     public static function getDIC(): array
     {
+        // TODO 'config/container.php' or extra.bootstrap ? А еще рядом есть ootstrap.php, в общем, надо разобраться, куда какие бутстрапы, может компонент придумать? Тут и консоль и очереди и свои DIC настройки
         return require __DIR__ . '/config/dic.php';
     }
+    public static function moduleId(): string { return self::MODULE_ID; }
+    public static function moduleVersion(): string { return self::VERSION; }
+    public static function isEditable(): bool { return self::EDITABLE; }
+    public static function adminMenu(): array { return require __DIR__.'/config/adminMenu.php'; }
+    public static function moduleConfig(): array { return require __DIR__.'/config/config.php'; }
+    public static function options(): array { return require __DIR__.'/config/options.php'; }
+    public static function migrationPath(): string { return __DIR__.'/migrations'; }
+    public static function migrationNamespace(): ?string { return __NAMESPACE__.'\\migrations'; }
+
 }
